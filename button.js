e80fc2ad0e46888f70cbc299a43698c262386679
mkangoo <alexanderpavlov1408@gmail.com>
2023-11-16 05:22:35 +0300

    feat: odd week

--- button.js
+++ button.js
@@ -5,9 +5,23 @@ const thursday = "Текст для кнопки 4";
 const friday = "Текст для кнопки 5";
 const saturday = "Текст для кнопки 6";
 
+const oddMonday = "Тест для Odd1";
+const oddTuesday = "Тест для Odd2";
+const oddWensday = "Тест для Odd3";
+const oddThursday = "Тест для Odd4";
+const oddFriday = "Тест для Odd5";
+const oddSaturday = "Тест для Odd6";
+
 module.exports.monday = monday;
 module.exports.tuesday = tuesday;
 module.exports.wensday = wensday;
 module.exports.thursday = thursday;
 module.exports.friday = friday;
 module.exports.saturday = saturday;
+
+module.exports.oddMonday = oddMonday;
+module.exports.oddTuesday = oddTuesday;
+module.exports.oddWensday = oddWensday;
+module.exports.oddThursday = oddThursday;
+module.exports.oddFriday = oddFriday;
+module.exports.oddSaturday = oddSaturday;
